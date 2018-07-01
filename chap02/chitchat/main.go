5656b01b989b8c33e4b5942d5dac705cc5dd4495
y.urakawa <s08a2015@gmail.com>
2018-07-02 00:35:21 +0900

    refactoring

--- chap02/chitchat/main.go
+++ chap02/chitchat/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"html/template"
+	"fmt"
 )
 
 func main() {
@@ -29,26 +30,22 @@ func main() {
 	server.ListenAndServe()
 }
 
+func generateHTML(w http.ResponseWriter, data interface{}, fn ...string) {
+	var files []string
+	for _, file := range fn {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+		templates := template.Must(template.ParseFiles(files...))
+		templates.ExecuteTemplate(w, "layout", data)
+	}
+}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	threads, err := data.Threads(); if err == nil {
 		_, err := session(w, r)
-		public_tmpl_files := []string{
-			"templates/layout.html",
-			"templates/public.navbar.html",
-			"templates/index.html",
-		}
-		private_tmpl_files := []string{
-			"templates/layout.html",
-			"templates/private.navbar.html",
-			"templates/index.html",
-		}
-		var templates *template.Template
 		if err != nil {
-			templates = template.Must(template.ParseFiles(private_tmpl_files...))
+			generateHTML(w, threads, "layout", "public.navbar", "index")
 		} else {
-			templates = template.Must(template.ParseFiles(public_tmpl_files...))
+			generateHTML(w, threads, "layout", "private.navbar", "index")
 		}
-		templates.ExecuteTemplate(w, "layout", threads)
 	}
 }

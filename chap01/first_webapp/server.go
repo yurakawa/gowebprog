dd210c287f30eac1338765c8e27a754b3dc75d0d
y.urakawa <s08a2015@gmail.com>
2018-07-01 17:33:31 +0900

    [list1-3] implement Hello World

---
+++ chap01/first_webapp/server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}

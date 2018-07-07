673a843a28dc62e71863cd67bf04e5674745109f
y.urakawa <s08a2015@gmail.com>
2018-07-08 01:20:06 +0900

    server handler

---
+++ chap03/s3-1/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+type MyHandler struct{}
+
+
+func (h *MyHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
+func main() {
+	handler := MyHandler{}
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+		Handler: &handler,
+	}
+
+	server.ListenAndServe()
+}
+

864a086c5ea9f65f015e62a80b7fb32eb1be6e7f
y.urakawa <s08a2015@gmail.com>
2018-07-16 01:30:38 +0900

    write json

--- chap04/11write/server.go
+++ chap04/11write/server.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"net/http"
+	"encoding/json"
 	"fmt"
+	"net/http"
 )
+
+type Post struct {
+	User    string
+	Threads []string
+}
 
 func writeExample(w http.ResponseWriter, r *http.Request) {
 	str := `<html>
@@ -23,6 +29,17 @@ func headerExample(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(302)
 }
 
+func jsonExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	post := &Post{
+		User:    "Sau Sheong",
+		Threads: []string{"1番目", "2番目", "3番目"},
+	}
+	json, _ := json.Marshal(post)
+	w.Write(json)
+}
+
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
@@ -30,5 +47,6 @@ func main() {
 	http.HandleFunc("/write", writeExample)
 	http.HandleFunc("/writeHeader", writeHeaderExample)
 	http.HandleFunc("/redirect", headerExample)
+	http.HandleFunc("/json", jsonExample)
 	server.ListenAndServe()
 }

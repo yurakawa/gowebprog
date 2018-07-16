608363c23dcf26979435d8c4283291f9dc3a3144
y.urakawa <s08a2015@gmail.com>
2018-07-16 22:08:40 +0900

    Use of cookies and cookies

--- chap04/16cookie/server.go
+++ chap04/16cookie/server.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"fmt"
+)
 
 func setCookie(w http.ResponseWriter, r *http.Request) {
 	c1 := http.Cookie{
@@ -18,10 +21,21 @@ func setCookie(w http.ResponseWriter, r *http.Request) {
 	http.SetCookie(w, &c2)
 }
 
+func getCookie(w http.ResponseWriter, r *http.Request) {
+	c1, err := r.Cookie("first_cookie")
+	if err != nil {
+		fmt.Fprintln(w, "Cannot get the first cookie")
+	}
+	cs := r.Cookies()
+	fmt.Fprintln(w, c1)
+	fmt.Fprintln(w, cs)
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/set_cookie", setCookie)
+	http.HandleFunc("/get_cookie", getCookie)
 	server.ListenAndServe()
 }

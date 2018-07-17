8b993637fb5b56ac4080af45555327dc7c3d80cd
y.urakawa <s08a2015@gmail.com>
2018-07-17 14:03:09 +0900

    template engine

---
+++ chap04/chap05/02trigger_template/server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+)
+
+func process(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("tmpl.html")
+	t.Execute(w, "Hello World!")
+}
+
+func main() {
+	server := http.Server {
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/process", process)
+	server.ListenAndServe()
+}
+

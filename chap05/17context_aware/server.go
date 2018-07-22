52c0b85b87ecb8786f04012c09fe61bf3ce92a12
y.urakawa <s08a2015@gmail.com>
2018-07-22 18:00:48 +0900

    context aware

---
+++ chap05/17context_aware/server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+)
+
+func process(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("tmpl.html")
+	content := `I asked <i> "What's up ?"</i>`
+	t.Execute(w, content)
+
+}
+
+func main() {
+	server := http.Server {
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/process", process)
+	server.ListenAndServe()
+}
+
+

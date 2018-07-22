f7e3c14d13281742d6f48e00f15ee15d397c47cf
y.urakawa <s08a2015@gmail.com>
2018-07-23 00:45:16 +0900

    block action

--- chap05/28nested/server.go
+++ chap05/28nested/server.go
@@ -15,10 +15,13 @@ func process(w http.ResponseWriter, r *http.Request) {
 func process2(w http.ResponseWriter, r *http.Request) {
 	rand.Seed(time.Now().Unix())
 	var t *template.Template
-	if rand.Intn(10) > 5 {
+	num := rand.Intn(10)
+	if num > 7 {
 		t, _ = template.ParseFiles("layout.html", "red_hello.html")
-	} else {
+	} else if (num > 3){
 		t, _ = template.ParseFiles("layout.html", "blue_hello.html")
+	} else {
+		t, _ = template.ParseFiles("layout.html")
 	}
 	t.ExecuteTemplate(w, "layout", "")
 

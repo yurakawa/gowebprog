c69c94d05d91265871c897a3ac77c16f3ee84e96
y.urakawa <s08a2015@gmail.com>
2018-07-26 01:29:53 +0900

    gob

---
+++ chap06/05gob_store/store.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/gob"
+	"bytes"
+	"io/ioutil"
+	"fmt"
+)
+
+type Post struct {
+	Id int
+	Content string
+	Author string
+}
+
+func store(data interface{}, filename string) {
+	buffer := new(bytes.Buffer)
+	encoder := gob.NewEncoder(buffer)
+	err := encoder.Encode(data)
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile(filename, buffer.Bytes(), 0600)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func load (data interface{}, filename string) {
+	raw, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	buffer := bytes.NewBuffer(raw)
+	dec := gob.NewDecoder(buffer)
+	err = dec.Decode(data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	post := Post{Id: 1, Content: "Hello World", Author:"Sau Sheong"}
+	store(post, "post1")
+	var postRead Post
+	load(&postRead, "post1")
+	fmt.Println(postRead)
+}
+

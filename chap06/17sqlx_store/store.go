5e2aecd1d2129453b42e683458af78035b8b4bbc
y.urakawa <s08a2015@gmail.com>
2018-07-28 14:55:28 +0900

    use sqlx

---
+++ chap06/17sqlx_store/store.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+type Post struct {
+	ID         int
+	Content    string
+	AuthorName string `db:"author"`
+}
+
+var Db *sqlx.DB
+
+func init() {
+	var err error
+	Db, err = sqlx.Open("postgres", "user=gwp dbname=gwp password=gwp sslmode=disable")
+	if err != nil {
+		panic(err)
+
+	}
+}
+
+func GetPost(id int) (post Post, err error) {
+	post = Post{}
+	err = Db.QueryRowx("selecdt id, content, author from posts where id = $1", id).StructScan(&post)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (post *Post) Create() (err error) {
+	err = Db.QueryRow("insert into posts (content, author) values ($1, $2) returning id", post.Content, post.AuthorName).Scan(&post.ID)
+	return
+}
+
+func main() {
+	post := Post{Content: "Hello World", AuthorName: "Sau Sheong"}
+	post.Create()
+	fmt.Println(post)
+	readPost := Post{}
+	readPost, _ = GetPost(post.ID)
+	fmt.Println(readPost)
+
+}

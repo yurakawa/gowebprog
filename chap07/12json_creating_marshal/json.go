b9bded3ebbd3671b02e18bf015acd9108e151fd6
y.urakawa <s08a2015@gmail.com>
2018-07-31 10:26:06 +0900

    json creating encoder

--- chap07/12json_creating_marshal/json.go
+++ chap07/12json_creating_marshal/json.go
@@ -3,57 +3,61 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
+	"os"
 )
 
 type Post struct {
-	ID       int       `json:"id"`
+	Id       int       `json:"id"`
 	Content  string    `json:"content"`
 	Author   Author    `json:"author"`
 	Comments []Comment `json:"comments"`
 }
 
 type Author struct {
-	ID   int    `json:"id"`
+	Id   int    `json:"id"`
 	Name string `json:"name"`
 }
 
 type Comment struct {
-	ID      int    `json:"id"`
+	Id      int    `json:"id"`
 	Content string `json:"content"`
 	Author  string `json:"author"`
 }
 
 func main() {
+
 	post := Post{
-		ID:      1,
+		Id:      1,
 		Content: "Hello World!",
 		Author: Author{
-			ID:   2,
+			Id:   2,
 			Name: "Sau Sheong",
 		},
 		Comments: []Comment{
 			{
-				ID:      1,
+				Id:      1,
 				Content: "Have a great day!",
 				Author:  "Adam",
 			},
 			{
-				ID:      2,
+				Id:      2,
 				Content: "How are you today?",
 				Author:  "Betty",
 			},
 		},
 	}
 
-	output, err := json.MarshalIndent(&post, "", "\t\t")
+	jsonFile, err := os.Create("post.json")
 	if err != nil {
-		fmt.Println("Error marshaling to JSON:", err)
+		fmt.Println("Error creating JSON file:", err)
 		return
 	}
-	err = ioutil.WriteFile("post.json", output, 0644)
+	jsonWriter := io.Writer(jsonFile)
+	encoder := json.NewEncoder(jsonWriter)
+	err = encoder.Encode(&post)
 	if err != nil {
-		fmt.Println("Error writing JSON to file", err)
+		fmt.Println("Error encoding JSON to file:", err)
 		return
 	}
 }

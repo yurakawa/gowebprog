da8ef0a6d4af921601f68a8b227ef60d4ee88cc0
yurakawa <s08a2015@gmail.com>
2018-11-04 16:12:14 +0900

    [Loc4654] GETリクエストのテストを追加

---
+++ chap08/09_httptest1/server_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/packer/common/json"
+)
+
+func TestHandleGet(t *testing.T) {
+	// 各テストケースは独立して実行されるため、それぞれにテスト用のWebサーバを立ち上げる必要がある
+	mux := http.NewServeMux()               // テストを実行するマルチプレクサを生成
+	mux.HandleFunc("/post/", handleRequest) // テスト対象のハンドラを追加
+
+	writer := httptest.NewRecorder()                     // 返されたHTTPレスポンスを取得
+	request, _ := http.NewRequest("GET", "/post/1", nil) // テストしたいハンドラ宛のリクエストを作成
+	mux.ServeHTTP(writer, request)                       // テスト対象のハンドラにリクエストを送信
+
+	if writer.Code != 200 { //ResponseRecoderにより結果をチェック
+		t.Errorf("Response code is %v", writer.Code)
+	}
+
+	var post Post
+	json.Unmarshal(writer.Body.Bytes(), &post)
+	if post.ID != 1 {
+		t.Error("Cannot retrieve JSON post")
+	}
+}
+
+}

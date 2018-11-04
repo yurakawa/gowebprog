4ed59fdd5d983137c767df41e40ebf9a72b815d9
yurakawa <s08a2015@gmail.com>
2018-11-04 16:12:35 +0900

    [Loc4654] PUTリクエストのテストを追加

--- chap08/09_httptest1/server_test.go
+++ chap08/09_httptest1/server_test.go
@@ -29,4 +29,16 @@ func TestHandleGet(t *testing.T) {
 	}
 }
 
+func TestHandlePut(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/post/", handleRequest)
+
+	writer := httptest.NewRecorder()
+	json := strings.NewReader(`{"content":"Updated post","author":"Sau Sheong"}`)
+	request, _ := http.NewRequest("PUT", "/post/1", json)
+	mux.ServeHTTP(writer, request)
+
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
 }

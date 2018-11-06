d455ed0cc1d23edbe00f0bcc82e7582280e18c13
yurakawa <s08a2015@gmail.com>
2018-11-06 20:59:42 +0900

    [Loc4862] パッケージcheckを使ったserver_test.go

---
+++ chap08/20gocheck_1/server_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/hashicorp/packer/common/json"
+
+	. "gopkg.in/check.v1"
+)
+
+type PostTestSuite struct{}
+
+func init() {
+	Suite(&PostTestSuite{})
+}
+
+func Test(t *testing.T) { TestingT(t) }
+
+func (s *PostTestSuite) TestHandleGet(c *C) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/post/", handleRequest(&FakePost{}))
+	writer := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/post/1", nil)
+	mux.ServeHTTP(writer, request)
+
+	c.Check(writer.Code, Equals, 200)
+	var post Post
+	json.Unmarshal(writer.Body.Bytes(), &post)
+	c.Check(post.ID, Equals, 1)
+}

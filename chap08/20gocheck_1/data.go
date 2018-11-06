999bc65573b1fdf97995fbf9662f86f8c2f3b46e
yurakawa <s08a2015@gmail.com>
2018-11-06 10:20:33 +0900

    [Loc4863] 18dependency_injectionのWebサービスをコピーする

---
+++ chap08/20gocheck_1/data.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+var Db *sql.DB
+
+type Text interface {
+	fetch(id int) (err error)
+	create() (err error)
+	update() (err error)
+	delete() (err error)
+}
+
+type Post struct {
+	Db      *sql.DB
+	ID      int    `json:"id"`
+	Content string `json:"content"`
+	Author  string `json:"author"`
+}
+
+// Get a single post
+func (post *Post) fetch(id int) (err error) {
+	err = post.Db.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.ID, &post.Content, &post.Author)
+	return
+}
+
+// Create a new post
+func (post *Post) create() (err error) {
+	statement := "insert into posts (content, author) values ($1, $2) returning id"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.ID)
+	return
+}
+
+// Update a post
+func (post *Post) update() (err error) {
+	_, err = Db.Exec("update posts set content = $2, author = $3 where id = $1", post.ID, post.Content, post.Author)
+	return
+}
+
+// Delete a post
+func (post *Post) delete() (err error) {
+	_, err = Db.Exec("delete from posts where id = $1", post.ID)
+	return
+}

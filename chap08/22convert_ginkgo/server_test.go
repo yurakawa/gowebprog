4860e9635b0763db9fc1a7fa912980c7e5993a50
yurakawa <s08a2015@gmail.com>
2018-11-07 20:59:52 +0900

    [Loc5020]  ginkgo convert . を実行後に 22convert_ginkgo から Ginkgo に置換

--- chap08/22convert_ginkgo/server_test.go
+++ chap08/22convert_ginkgo/server_test.go
@@ -5,39 +5,43 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"strings"
-	"testing"
+
+	. "github.com/onsi/ginkgo"
 )
 
-func TestHandleGet(t *testing.T) {
-	// 各テストケースは独立して実行されるため、それぞれにテスト用のWebサーバを立ち上げる必要がある
-	mux := http.NewServeMux()                            // テストを実行するマルチプレクサを生成
-	mux.HandleFunc("/post/", handleRequest(&FakePost{})) // テスト対象のハンドラを追加
+var _ = Describe("Testing with Ginkgo", func() {
+	It("get post", func() {
 
-	writer := httptest.NewRecorder()                     // 返されたHTTPレスポンスを取得
-	request, _ := http.NewRequest("GET", "/post/1", nil) // テストしたいハンドラ宛のリクエストを作成
-	mux.ServeHTTP(writer, request)                       // テスト対象のハンドラにリクエストを送信
+		mux := http.NewServeMux()
+		mux.HandleFunc("/post/", handleRequest(&FakePost{}))
 
-	if writer.Code != 200 { //ResponseRecoderにより結果をチェック
-		t.Errorf("Response code is %v", writer.Code)
-	}
+		writer := httptest.NewRecorder()
+		request, _ := http.NewRequest("GET", "/post/1", nil)
+		mux.ServeHTTP(writer, request)
 
-	var post Post
-	json.Unmarshal(writer.Body.Bytes(), &post)
-	if post.ID != 1 {
-		t.Error("Cannot retrieve JSON post")
-	}
-}
+		if writer.Code != 200 {
+			GinkgoT().Errorf("Response code is %v", writer.Code)
+		}
 
-func TestHandlePut(t *testing.T) {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/post/", handleRequest(&FakePost{}))
+		var post Post
+		json.Unmarshal(writer.Body.Bytes(), &post)
+		if post.ID != 1 {
+			GinkgoT().Error("Cannot retrieve JSON post")
+		}
+	})
+	It("put post", func() {
 
-	writer := httptest.NewRecorder()
-	json := strings.NewReader(`{"content":"Updated post","author":"Sau Sheong"}`)
-	request, _ := http.NewRequest("PUT", "/post/1", json)
-	mux.ServeHTTP(writer, request)
+		mux := http.NewServeMux()
+		post := &FakePost{}
+		mux.HandleFunc("/post/", handleRequest(post))
 
-	if writer.Code != 200 {
-		t.Errorf("Response code is %v", writer.Code)
-	}
-}
+		writer := httptest.NewRecorder()
+		json := strings.NewReader(`{"content":"Updated post","author":"Sau Sheong"}`)
+		request, _ := http.NewRequest("PUT", "/post/1", json)
+		mux.ServeHTTP(writer, request)
+
+		if writer.Code != 200 {
+			GinkgoT().Errorf("Response code is %v", writer.Code)
+		}
+	})
+})

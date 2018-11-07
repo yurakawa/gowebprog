f628bccf4688539631f614fb7b2cc98d5c4fb917
yurakawa <s08a2015@gmail.com>
2018-11-07 21:11:22 +0900

    [Loc5071] 22convert_ginkgo からコピー

---
+++ chap08/25test_ginkgo/doubles.go
@@ -0,0 +1,24 @@
+package main
+
+type FakePost struct {
+	ID      int
+	Content string
+	Author  string
+}
+
+func (post *FakePost) fetch(id int) (err error) {
+	post.ID = id
+	return
+}
+
+func (post *FakePost) create() (err error) {
+	return
+}
+
+func (post *FakePost) update() (err error) {
+	return
+}
+
+func (post *FakePost) delete() (err error) {
+	return
+}
---
+++ chap08/25test_ginkgo/server.go
@@ -0,0 +1,131 @@
+/*
+
+# docekrコンテナ起動
+docker container run -d --rm --name some-postgres -e POSTGRES_USER=gwp -e POSTGRES_PASSWORD=gwp -p 5432:5432 postgres
+
+#不要# psql -h localhost -U gwp -f install.sql
+psql -h localhost -U gwp -f setup.sql
+
+go build
+
+./server
+./script_create                                            # curlを実行
+psql -h localhost  -U gwp -d gwp -c "select * from posts;" # レコードが作成されたことを確認
+
+*/
+
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"path"
+	"strconv"
+)
+
+func main() {
+
+	var err error
+	db, err := sql.Open("postgres", "user=gwp dbname=gwp sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	server := http.Server{
+		Addr: ":8080",
+	}
+	http.HandleFunc("/post/", handleRequest(&Post{Db: db}))
+	server.ListenAndServe()
+}
+
+func handleRequest(t Text) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		switch r.Method {
+		case "GET":
+			err = handleGet(w, r, t)
+		case "POST":
+			err = handlePost(w, r, t)
+		case "PUT":
+			err = handlePut(w, r, t)
+		case "DELETE":
+			err = handleDelete(w, r, t)
+		}
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request, post Text) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		return
+	}
+	err = post.fetch(id)
+	if err != nil {
+		return
+	}
+	output, err := json.MarshalIndent(&post, "", "\t\t")
+	if err != nil {
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	// http.NotFound(w, r)
+	return
+}
+
+func handlePost(w http.ResponseWriter, r *http.Request, post Text) (err error) {
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+	json.Unmarshal(body, &post)
+	err = post.create()
+	if err != nil {
+		return
+	}
+	w.WriteHeader(200)
+	return
+}
+
+func handlePut(w http.ResponseWriter, r *http.Request, post Text) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		return
+	}
+
+	err = post.fetch(id)
+	if err != nil {
+		return
+	}
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+	json.Unmarshal(body, &post)
+	err = post.update()
+	if err != nil {
+		return
+	}
+	w.WriteHeader(200)
+	return
+}
+
+func handleDelete(w http.ResponseWriter, r *http.Request, post Text) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		return
+	}
+	err = post.fetch(id)
+	if err != nil {
+		return
+	}
+	err = post.delete()
+	if err != nil {
+		return
+	}
+	w.WriteHeader(200)
+	return
+}

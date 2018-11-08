f45f9d0284d60d39a88ef02cfe94b4def3da7263
yurakawa <s08a2015@gmail.com>
2018-11-08 20:42:30 +0900

    [Loc5240] ゴルーチンになっている関数となっていない関数のベンチマーク

--- chap09/1goroutine/goroutine1_test.go
+++ chap09/1goroutine/goroutine1_test.go
@@ -19,3 +19,15 @@ func TestGoPrint2(t *testing.T) {
 	goPrint2()
 	time.Sleep(1 * time.Millisecond)
 }
+
+func BenchmarkPrint1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		print1()
+	}
+}
+
+func BenchmarkGoPrint1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		goPrint1()
+	}
+}

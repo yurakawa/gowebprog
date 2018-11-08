6b52799b2bc97f5ffa26ae1c88a59875e1ae4adb
yurakawa <s08a2015@gmail.com>
2018-11-09 00:59:57 +0900

    [Loc5260] 100回繰り返しに増量してgoroutineの効果の推移を確認する

--- chap09/1goroutine/goroutine1.go
+++ chap09/1goroutine/goroutine1.go
@@ -5,13 +5,13 @@ import (
 )
 
 func printNumbers1() {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 100; i++ {
 		// fmt.Printf("%d ", i)
 	}
 }
 
 func printLetters1() {
-	for i := 'A'; i < 'A'+10; i++ {
+	for i := 'A'; i < 'A'+100; i++ {
 		// fmt.Printf("%c ", i)
 	}
 }
@@ -27,14 +27,14 @@ func goPrint1() {
 }
 
 func printNumbers2() {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 100; i++ {
 		time.Sleep(1 * time.Microsecond)
 		// fmt.Printf("%d ", i)
 	}
 }
 
 func printLetters2() {
-	for i := 'A'; i < 'A'+10; i++ {
+	for i := 'A'; i < 'A'+100; i++ {
 		time.Sleep(1 * time.Microsecond)
 		// fmt.Printf("%c ", i)
 	}

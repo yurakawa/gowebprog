c0106ada374a915046024a365c0d64771c4abecc
yurakawa <s08a2015@gmail.com>
2018-11-09 10:27:26 +0900

    [Loc5403] チャネルを使用したメッセージの伝達

---
+++ chap09/08channel_message/channel.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func thrower(c chan int) {
+	for i := 0; i < 5; i++ {
+		c <- i
+		fmt.Println("Threw >>", i)
+	}
+}
+
+func cacher(c chan int) {
+	for i := 0; i < 5; i++ {
+		num := <-c
+		fmt.Println("Caught <<", num)
+	}
+}
+
+func main() {
+	c := make(chan int)
+	go thrower(c)
+	go cacher(c)
+	time.Sleep(100 * time.Millisecond)
+}

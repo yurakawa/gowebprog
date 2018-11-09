cad6c47e1ad6e9da6e1431b7291bff81e043fea8
yurakawa <s08a2015@gmail.com>
2018-11-09 20:28:01 +0900

    [Loc5483] チャネルを閉じる

--- chap09/09channel_select/channel.go
+++ chap09/09channel_select/channel.go
@@ -2,30 +2,34 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func callerA(c chan string) {
 	c <- "Hello World"
+	close(c)
 }
 
 func callerB(c chan string) {
 	c <- "HolaMundo!"
+	close(c)
 }
 
 func main() {
 	a, b := make(chan string), make(chan string)
 	go callerA(a)
 	go callerB(b)
-	for i := 0; i < 5; i++ {
-		time.Sleep(1 * time.Millisecond)
+	var msg string
+	openA, openB := true, true
+	for openA || openB {
 		select {
-		case msg := <-a:
-			fmt.Printf("%s from A\n", msg)
-		case msg := <-b:
-			fmt.Printf("%s from B\n", msg)
-		default:
-			fmt.Println("Default")
+		case msg, openA = <-a:
+			if openA {
+				fmt.Printf("%s from A\n", msg)
+			}
+		case msg, openB = <-b:
+			if openB {
+				fmt.Printf("%s from B\n", msg)
+			}
 		}
 	}
 

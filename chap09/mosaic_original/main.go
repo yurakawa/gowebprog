7ebd9b1fcab5416e07d5ba1bdfd0f558e12dcbd8
yurakawa <s08a2015@gmail.com>
2018-11-12 10:27:54 +0900

    [Loc5671] 高速化のため画像を4分割し並列化する

--- chap09/mosaic_original/main.go
+++ chap09/mosaic_original/main.go
@@ -38,6 +38,7 @@ func upload(w http.ResponseWriter, r *http.Request) {
 func mosaic(w http.ResponseWriter, r *http.Request) {
 	t0 := time.Now()
 
+	// bodyのメモリ上の最大容量は10MB
 	r.ParseMultipartForm(10485760)
 
 	// アップロードされたファイルとタイルサイズを取得
@@ -49,53 +50,29 @@ func mosaic(w http.ResponseWriter, r *http.Request) {
 	original, _, _ := image.Decode(file)
 	bounds := original.Bounds()
 
-	newimage := image.NewRGBA(image.Rect(bounds.Min.X, bounds.Min.X, bounds.Max.X, bounds.Max.Y))
+	// newimage := image.NewRGBA(image.Rect(bounds.Min.X, bounds.Min.X, bounds.Max.X, bounds.Max.Y))
 
 	// タイル画像データベースを複製
 	db := cloneTilesDB()
 
-	// 各タイルの基準ピクセルを設定
-	sp := image.Point{0, 0}
+	// 独立して処理できるように画像を分割して処理を分散(ファン・アウト)
+	c1 := cut(original, &db, tileSize, bounds.Min.X, bounds.Min.Y, bounds.Max.X/2, bounds.Max.Y/2)
+	c2 := cut(original, &db, tileSize, bounds.Min.X/2, bounds.Min.Y, bounds.Max.X, bounds.Max.Y/2)
+	c3 := cut(original, &db, tileSize, bounds.Min.X, bounds.Min.Y/2, bounds.Max.X/2, bounds.Max.Y)
+	c4 := cut(original, &db, tileSize, bounds.Min.X/2, bounds.Min.Y/2, bounds.Max.X, bounds.Max.Y)
 
-	// ターゲット画像を巡回
-	for y := bounds.Min.Y; y < bounds.Max.Y; y = y + tileSize {
-		for x := bounds.Min.X; x < bounds.Max.X; x = x + tileSize {
-
-			r, g, b, _ := original.At(x, y).RGBA()
-			color := [3]float64{float64(r), float64(g), float64(b)}
-
-			nearest := nearest(color, &db)
-			file, err := os.Open(nearest)
-			if err == nil {
-				img, _, err := image.Decode(file)
-				if err == nil {
-
-					t := resize(img, tileSize)
-					tile := t.SubImage(t.Bounds())
-					tileBounds := image.Rect(x, y, x+tileSize, y+tileSize)
-					draw.Draw(newimage, tileBounds, tile, sp, draw.Src)
-				} else {
-					fmt.Println("error:", err, nearest)
-				}
-			} else {
-				fmt.Println("error:", nearest)
-			}
-			file.Close()
-		}
-	}
+	// 画像をつなげて処理結果をまとめる
+	c := combine(bounds, c1, c2, c3, c4)
 
 	// JPEGにエンコードし、base64文字列に変更してブラウザに送信
 	buf1 := new(bytes.Buffer)
 	jpeg.Encode(buf1, original, nil)
 	originalStr := base64.StdEncoding.EncodeToString(buf1.Bytes())
 
-	buf2 := new(bytes.Buffer)
-	jpeg.Encode(buf2, newimage, nil)
-	mosaic := base64.StdEncoding.EncodeToString(buf2.Bytes())
 	t1 := time.Now()
 	images := map[string]string{
 		"original": originalStr,
-		"mosaic":   mosaic,
+		"mosaic":   <-c
 		"duration": fmt.Sprintf("%v", t1.Sub(t0)),
 	}
 	t, _ := template.ParseFiles("results.html")
